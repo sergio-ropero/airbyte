f9adeaa417e03ecd3fbc22c8947b28b5a6ace1d1
Marius Posta <marius@airbyte.io>
2024-04-25 23:07:33 -0400

    cdk2: add CHECK and DISCOVER operations

--- airbyte-integrations/connectors/source-oracle-v2/src/cdk/test/kotlin/io/airbyte/cdk/test/source/TestSourceIntegrationTest.kt
+++ airbyte-integrations/connectors/source-oracle-v2/src/cdk/test/kotlin/io/airbyte/cdk/test/source/TestSourceIntegrationTest.kt
@@ -14,12 +14,18 @@ import com.deblock.jsondiff.matcher.StrictPrimitivePartialMatcher
 import com.deblock.jsondiff.viewer.OnlyErrorDiffViewer
 import io.airbyte.cdk.command.CliRunner
 import io.airbyte.cdk.consumers.BufferingOutputConsumer
+import io.airbyte.cdk.jdbc.H2TestFixture
 import io.airbyte.cdk.operation.OperationType
+import io.airbyte.cdk.ssh.SshBastionContainer
+import io.airbyte.cdk.testcontainers.DOCKER_HOST_FROM_WITHIN_CONTAINER
 import io.airbyte.commons.json.Jsons
 import io.airbyte.commons.resources.MoreResources
+import io.airbyte.protocol.models.v0.AirbyteCatalog
+import io.airbyte.protocol.models.v0.AirbyteConnectionStatus
 import io.airbyte.protocol.models.v0.ConnectorSpecification
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.testcontainers.Testcontainers
 
 class TestSourceIntegrationTest {
 
@@ -44,4 +50,78 @@ class TestSourceIntegrationTest {
             ConnectorSpecification::class.java
         )
 
+    val h2 = H2TestFixture()
+    init {
+        Testcontainers.exposeHostPorts(h2.port)
+        h2.execute(
+            """CREATE TABLE kv (
+            |k INT PRIMARY KEY, 
+            |v VARCHAR(60))
+            |"""
+                .trimMargin()
+                .replace('\n', ' ')
+        )
+        h2.execute(
+            """CREATE TABLE events (
+            |id UUID GENERATED ALWAYS AS (RANDOM_UUID()) PRIMARY KEY,
+            |ts TIMESTAMP WITH TIME ZONE NOT NULL, 
+            |msg VARCHAR(60))
+            |"""
+                .trimMargin()
+                .replace('\n', ' ')
+        )
+    }
+    val sshBastion = SshBastionContainer(tunnelingToHostPort = h2.port)
+
+    @Test
+    fun testCheckAndDiscoverNoTunnel() {
+        testCheckAndDiscover(
+            TestSourceConfigurationJsonObject().apply {
+                port = h2.port
+                database = h2.database
+            }
+        )
+    }
+
+    @Test
+    fun testCheckAndDiscoverSshKeyAuth() {
+        testCheckAndDiscover(
+            TestSourceConfigurationJsonObject().apply {
+                host = DOCKER_HOST_FROM_WITHIN_CONTAINER // required only because of container
+                port = h2.port
+                database = h2.database
+                setTunnelMethodValue(sshBastion.outerKeyAuthTunnelMethod)
+            }
+        )
+    }
+
+    @Test
+    fun testCheckAndDiscoverSshPasswordAuth() {
+        testCheckAndDiscover(
+            TestSourceConfigurationJsonObject().apply {
+                host = DOCKER_HOST_FROM_WITHIN_CONTAINER // required only because of container
+                port = h2.port
+                database = h2.database
+                setTunnelMethodValue(sshBastion.outerPasswordAuthTunnelMethod)
+            }
+        )
+    }
+
+    private fun testCheckAndDiscover(configPojo: TestSourceConfigurationJsonObject) {
+        val checkOutput: BufferingOutputConsumer =
+            CliRunner.runSource(OperationType.CHECK, configPojo)
+        Assertions.assertEquals(
+            listOf(AirbyteConnectionStatus().withStatus(AirbyteConnectionStatus.Status.SUCCEEDED)),
+            checkOutput.statuses()
+        )
+        val discoverOutput: BufferingOutputConsumer =
+            CliRunner.runSource(OperationType.DISCOVER, configPojo)
+        Assertions.assertEquals(listOf(expectedCatalog), discoverOutput.catalogs())
+    }
+
+    val expectedCatalog: AirbyteCatalog =
+        Jsons.deserialize(
+            MoreResources.readResource("test/source/expected-catalog.json"),
+            AirbyteCatalog::class.java
+        )
 }
